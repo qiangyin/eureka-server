95c19e87f70b316fe41bc1d301dc32f42bd905b3
yintongjiang <261026193@qq.com>
2018-04-04 13:33:30 +0800

    init

---
+++ src/main/java/com/EurekaServer.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author yintongjiang
+ * @params
+ * @since 2018/2/28
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
